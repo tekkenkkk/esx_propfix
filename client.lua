9d212c8c1cc7311a6d2cd8672aab2255894e26cd
tekken420weed <74188150+tekken420weed@users.noreply.github.com>
2021-06-24 00:22:32 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,14 +1,24 @@
-RegisterNetEvent('esx_propfix:Use')
-AddEventHandler('esx_propfix:Use', function()
-	TriggerEvent('skinchanger:getSkin', function(skin)
-		if skin.sex == 0 then
-			TriggerEvent('skinchanger:loadSkin', {sex=1})
-			Citizen.Wait(3000)
-			TriggerEvent('skinchanger:loadSkin', {sex=0})
-		elseif skin.sex == 1 then
-			TriggerEvent('skinchanger:loadSkin', {sex=0})
-			Citizen.Wait(3000)
-			TriggerEvent('skinchanger:loadSkin', {sex=1})
-		end
-	end)
+local wait = false
+
+RegisterCommand("propfix", function()
+    if not wait then
+       TriggerEvent('skinchanger:getSkin', function(skin)
+        wait = true
+        local hp = GetEntityHealth(GetPlayerPed(-1))
+        if skin.sex == 0 then
+            TriggerEvent('skinchanger:loadSkin', {sex=1})
+            Citizen.Wait(1000)
+            TriggerEvent('skinchanger:loadSkin', {sex=0})
+        elseif skin.sex == 1 then
+            TriggerEvent('skinchanger:loadSkin', {sex=0})
+            Citizen.Wait(1000)
+            TriggerEvent('skinchanger:loadSkin', {sex=1})
+        end
+        SetEntityHealth(GetPlayerPed(-1), hp)
+        Wait(30000)
+        wait = false
+        end)
+    else
+	ESX.ShowNotification('Nie możesz używac tej komendy tak często')
+    end
 end)
