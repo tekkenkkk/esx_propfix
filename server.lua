f94844903497376e7468e35771872761dc2d8838
tekken420weed <74188150+tekken420weed@users.noreply.github.com>
2021-06-24 00:15:56 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,12 +1,12 @@
 local wait = false
 
 RegisterCommand("propfix", function(source)
-    if not wait then
+	if not wait then
 		wait = true
 		TriggerClientEvent('esx_propfix:Use', source)
 		Wait(30000)
 		wait = false
 	else
-        TriggerClientEvent('esx:showNotification', source, 'Nie możesz używac tej komendy tak często')
+		TriggerClientEvent('esx:showNotification', source, 'Nie możesz używac tej komendy tak często')
 	end
 end, false)
